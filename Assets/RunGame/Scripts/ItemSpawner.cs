12659d9d023b604cda0e3e88e777d50ee02f5dbe
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-08-05 15:52:50 +0900

    ItemSpaener Provisional Completion

--- Assets/RunGame/Scripts/ItemSpawner.cs
+++ Assets/RunGame/Scripts/ItemSpawner.cs
@@ -8,9 +8,20 @@ public class ItemSpawner : MonoBehaviour
     [SerializeField] GameObject powerUPItem;
     [SerializeField] GameObject[] coinsPrefab;
     [SerializeField] Transform[] spawnPoint;
+    [SerializeField] int _level;
+    Transform[] point;
 
     void Start()
     {
-        
+        point = new Transform[10 + _level / 2];
+        for(int i = 0; i < point.Length; i++)
+        {
+            point[i] = spawnPoint[Random.Range(0,spawnPoint.Length)];
+        }
+        for (int i = 0; i < point.Length; i++)
+        {
+            var item = Instantiate(coinsPrefab[Random.Range(0, coinsPrefab.Length)],point[i]);
+            
+        }
     }
 }
