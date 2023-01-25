74c43cb65dfae6043414895c3ad6d33612db482a
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2023-01-25 13:42:14 +0900

    Randomize Item Count

--- Assets/RunGame/Scripts/PowerUP.cs
+++ Assets/RunGame/Scripts/PowerUP.cs
@@ -5,12 +5,14 @@ public class PowerUP : Item
 {
     [SerializeField] GameObject childRunner;
     [SerializeField] Transform spawnPos;
-    [SerializeField] int count;
+    [SerializeField] int maxCount;
     [SerializeField] TextMeshPro text;
     [SerializeField] GameObject spawnPoint;
+    int count;
     private void Start()
     {
         text.GetComponent<TextMeshPro>();
+        count = Random.Range(1, maxCount);
         text.text = count.ToString();
     }
     public override void Action()
